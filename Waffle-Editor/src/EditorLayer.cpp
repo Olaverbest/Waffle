d44bebd6d4e103bb098de2c3e3ce8a338735506f
Olaverbest <olaverbestbest@gmail.com>
2024-08-27 23:49:07 +0200

    Fixed resizing bugs

--- Waffle-Editor/src/EditorLayer.cpp
+++ Waffle-Editor/src/EditorLayer.cpp
@@ -35,6 +35,15 @@ namespace Waffle {
 	void EditorLayer::OnUpdate(Waffle::Timestep ts)
 	{
 		WF_PROFILE_FUNCTION();
+
+		if (Waffle::FramebufferSpecification spec = m_Framebuffer->GetSpecification();
+			m_ViewportSize.x > 0.0f && m_ViewportSize.y > 0.0f &&
+			(spec.Width != m_ViewportSize.x || spec.Height != m_ViewportSize.y))
+		{
+			m_Framebuffer->Resize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
+			m_CameraController.OnResize(m_ViewportSize.x, m_ViewportSize.y);
+		}
+
 		// Update
 		if (m_ViewportFocused)
 			m_CameraController.OnUpdate(ts);
@@ -132,7 +141,6 @@ namespace Waffle {
 		}
 
 		// RENDER SETTINGS
-
 		ImGui::Begin("Settings");
 
 		auto stats = Renderer2D::GetStats();
@@ -153,13 +161,8 @@ namespace Waffle {
 		Application::Get().GetImGuiLayer()->BlockEvents(!m_ViewportFocused || !m_ViewportHovered);
 
 		ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
-		if (m_ViewportSize != *((glm::vec2*)&viewportPanelSize))
-		{
-			m_Framebuffer->Resize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
-			m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
+		m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
 
-			m_CameraController.OnResize(viewportPanelSize.x, viewportPanelSize.y);
-		}
 		uint32_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
 		ImGui::Image((void*)textureID, ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
 		ImGui::End();
