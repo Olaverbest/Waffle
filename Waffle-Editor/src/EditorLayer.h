b1887140f3cee08a021ad2c60515f18ece79c23b
Olaverbest <olaverbestbest@gmail.com>
2024-08-31 23:04:20 +0200

    Refactored framebuffer, and prepared framebuffers for picking entities with the mouse

--- Waffle-Editor/src/EditorLayer.h
+++ Waffle-Editor/src/EditorLayer.h
@@ -42,6 +42,7 @@ namespace Waffle {
 
 		bool m_ViewportFocused = false, m_ViewportHovered = false;
 		glm::vec2 m_ViewportSize = { 0, 0 };
+		glm::vec2 m_ViewportBounds[2];
 
 		int m_GizmoType = -1;
 
